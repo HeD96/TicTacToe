fb5ba65dae335b977a63e2335f75ee6fc32130d2
imhed <hedplanet96@gmail.com>
2023-06-27 23:11:19 +0300

    added function that checks marked indices with win combinations

--- index.js
+++ index.js
@@ -15,7 +15,12 @@ const Gameboard = (function () {
             cell.classList.add("cell");
             gameboard.appendChild(cell);
             cell.addEventListener("click", function () {
-                Gameplay.currentTurn(cell, i);
+                if (
+                    !playerOne.indices.includes(i) &&
+                    !playerTwo.indices.includes(i)
+                ) {
+                    Gameplay.currentTurn(cell, i);
+                }
             });
 
             gameArray.push(cell);
@@ -29,19 +34,57 @@ const Gameboard = (function () {
 
 const Gameplay = (function () {
     let turn = 0;
+    let hasAI = false;
+    let winCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
 
     let currentTurn = function (cell, i) {
         if (turn === 0) {
             playerOne.markCell(cell, i);
             turn = 1;
-        } else if (turn === 1) {
+        } else if (turn === 1 && hasAI === false) {
             playerTwo.markCell(cell, i);
             turn = 0;
         }
     };
 
+    let arrayCompare = function (indices) {
+        let winArr = [];
+
+        for (let i = 0; i < winCombos.length; i++) {
+            const win = winCombos[i].every(function (value) {
+                return indices.includes(value);
+            });
+            winArr.push(win);
+        }
+
+        return winArr;
+    };
+
+    let winCheckOne = function (indicesArray) {
+        if (arrayCompare(indicesArray).includes(true)) {
+            alert("player one win!");
+        }
+    };
+
+    let winCheckTwo = function (indicesArray) {
+        if (arrayCompare(indicesArray).includes(true)) {
+            alert("player two win!");
+        }
+    };
+
     return {
         currentTurn,
+        winCheckOne,
+        winCheckTwo,
     };
 })();
 
@@ -56,9 +99,11 @@ function Player(playerState, isAI) {
         if (player === "one") {
             cell.innerHTML = _xmark;
             indices.push(i);
+            Gameplay.winCheckOne(indices);
         } else if (player === "two") {
             cell.innerHTML = _omark;
             indices.push(i);
+            Gameplay.winCheckTwo(indices);
         }
     };
 
@@ -70,105 +115,3 @@ function Player(playerState, isAI) {
 
 const playerOne = Player("one", false);
 const playerTwo = Player("two", false);
-
-// window.onload = function () {
-//     gameBoard.startMenu();
-// };
-
-// const wrapper = document.createElement("div");
-// wrapper.classList.add("wrapper");
-// document.body.appendChild(wrapper);
-
-// function Player(playerState, isAI) {
-//     let player = playerState;
-//     let ai = isAI;
-//     let score = 0;
-
-//     return {
-//         player,
-//         ai,
-//         score,
-//     };
-// }
-
-// const gameBoard = (function () {
-//     let hasAI;
-//     let startMenu = function () {
-//         const aiPlayer = document.createElement("button");
-//         aiPlayer.classList.add("opponent_button", "one");
-//         wrapper.appendChild(aiPlayer);
-//         aiPlayer.innerText = "One Player";
-//         aiPlayer.addEventListener("click", function () {
-//             hasAI = true;
-//             gamePlay.createPlayer(hasAI);
-//         });
-
-//         const secondPlayer = document.createElement("button");
-//         secondPlayer.classList.add("opponent_button", "two");
-//         wrapper.appendChild(secondPlayer);
-//         secondPlayer.innerText = "Two Players";
-//         secondPlayer.addEventListener("click", function () {
-//             hasAI = false;
-//             gamePlay.createPlayer(hasAI);
-//         });
-
-//         const startButton = document.createElement("button");
-//         startButton.classList.add("start_button");
-//         startButton.addEventListener("click", setGameboard);
-//         wrapper.appendChild(startButton);
-//         startButton.innerText = "Start Game";
-//     };
-
-//     let setGameboard = function () {
-//         const gameboard = document.createElement("div");
-//         gameboard.classList.add("gameboard");
-//         wrapper.appendChild(gameboard);
-
-//         let cells = [];
-
-//         for (let i = 0; i < 9; i++) {
-//             let cell = document.createElement("div");
-//             cell.classList.add("cell");
-//             gameboard.appendChild(cell);
-
-//             cells.push(cell);
-//         }
-
-//         gamePlay.markCell(cells);
-//     };
-
-//     return {
-//         startMenu,
-//     };
-// })();
-
-// const gamePlay = (function () {
-//     let _xmark = '<i class="fa-solid fa-xmark" style="color: #c70000;"></i>';
-//     let _omark = '<i class="fa-solid fa-o" style="color: #525fe1;"></i>';
-//     let turn = 0;
-
-//     let createPlayer = function (ai) {
-//         if (ai === true) {
-//         } else if (ai === false) {
-//         }
-//     };
-
-//     let markCell = function (cellArr) {
-//         cellArr.forEach(function (cell) {
-//             cell.addEventListener("click", function () {
-//                 if (turn === 0) {
-//                     cell.innerHTML = _xmark;
-//                     turn = 1;
-//                 } else if (turn === 1) {
-//                     cell.innerHTML = _omark;
-//                     turn = 0;
-//                 }
-//             });
-//         });
-//     };
-
-//     return {
-//         markCell,
-//         createPlayer,
-//     };
-// })();
